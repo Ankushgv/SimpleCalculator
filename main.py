f4ed1a7a254bee39a3366fcf7414818e446b5b79
Ankush <kush.agv@gmail.com>
2024-10-06 13:26:59 +0200

    changing the layout

--- main.py
+++ main.py
@@ -20,14 +20,41 @@ class MainWindow(QMainWindow):
         # self.inputField  ## to accept only numbers and signs
         self.zeroRow = QHBoxLayout()
         self.inputField.textChanged.connect(self.displayValue.setText)
+        self.number1 = QPushButton("1")
+        self.number1.clicked.connect(self.clickAction)
+
+        self.number2 = QPushButton("2")
+        self.number2.clicked.connect(self.clickAction)
+
+        self.number3 = QPushButton("3")
+        self.number3.clicked.connect(self.clickAction)
+
         self.clearButton = QPushButton("Clear")
         self.clearButton.clicked.connect(self.clearEntry)
 
         self.deleteButton = QPushButton("del")
         self.deleteButton.clicked.connect(self.backspace)
 
-        self.equals = QPushButton("=")
-        self.equals.clicked.connect(self.clickAction)
+        self.mainDisplayScreen.addWidget(self.displayValue)
+        self.mainDisplayScreen.addWidget(self.inputField)
+        
+        self.zeroRow.addWidget(self.number1)
+        self.zeroRow.addWidget(self.number2)
+        self.zeroRow.addWidget(self.number3)
+        self.zeroRow.addWidget(self.clearButton)
+        self.zeroRow.addWidget(self.deleteButton)
+        self.mainDisplayScreen.addLayout(self.zeroRow)
+
+        self.firstRow = QHBoxLayout()
+        
+        self.number4 = QPushButton("4")
+        self.number4.clicked.connect(self.clickAction)
+
+        self.number5 = QPushButton("5")
+        self.number5.clicked.connect(self.clickAction)
+
+        self.number6 = QPushButton("6")
+        self.number6.clicked.connect(self.clickAction)
 
         self.openBracket = QPushButton("(")
         self.openBracket.clicked.connect(self.clickAction)
@@ -35,55 +62,36 @@ class MainWindow(QMainWindow):
         self.closeBracket = QPushButton(")")
         self.closeBracket.clicked.connect(self.clickAction)
 
-        self.mainDisplayScreen.addWidget(self.displayValue)
-        self.mainDisplayScreen.addWidget(self.inputField)
-        
-        self.zeroRow.addWidget(self.clearButton)
-        self.zeroRow.addWidget(self.deleteButton)
-        self.zeroRow.addWidget(self.equals)
-        self.zeroRow.addWidget(self.openBracket)
-        self.zeroRow.addWidget(self.closeBracket)
-        self.mainDisplayScreen.addLayout(self.zeroRow)
-
-        self.firstRow = QHBoxLayout()
-        
-        self.number1 = QPushButton("1")
-        self.number1.clicked.connect(self.clickAction)
-        self.number2 = QPushButton("2")
-        self.number2.clicked.connect(self.clickAction)
-        self.number3 = QPushButton("3")
-        self.number3.clicked.connect(self.clickAction)
-        self.number4 = QPushButton("4")
-        self.number4.clicked.connect(self.clickAction)
-        self.number5 = QPushButton("5")
-        self.number5.clicked.connect(self.clickAction)
-
-        self.firstRow.addWidget(self.number1)
-        self.firstRow.addWidget(self.number2)
-        self.firstRow.addWidget(self.number3)
         self.firstRow.addWidget(self.number4)
         self.firstRow.addWidget(self.number5)
+        self.firstRow.addWidget(self.number6)
+        self.firstRow.addWidget(self.openBracket)
+        self.firstRow.addWidget(self.closeBracket)
 
         self.mainDisplayScreen.addLayout(self.firstRow)
 
         self.secondRow = QHBoxLayout()
         
-        self.number6 = QPushButton("6")
-        self.number6.clicked.connect(self.clickAction)
         self.number7 = QPushButton("7")
         self.number7.clicked.connect(self.clickAction)
+
         self.number8 = QPushButton("8")
         self.number8.clicked.connect(self.clickAction)
+
         self.number9 = QPushButton("9")
         self.number9.clicked.connect(self.clickAction)
-        self.number0 = QPushButton("0")
-        self.number0.clicked.connect(self.clickAction)
 
-        self.secondRow.addWidget(self.number6)
+        self.numberPlus = QPushButton("+")
+        self.numberPlus.clicked.connect(self.clickAction)
+
+        self.numberMinus = QPushButton("-")
+        self.numberMinus.clicked.connect(self.clickAction)
+
         self.secondRow.addWidget(self.number7)
         self.secondRow.addWidget(self.number8)
         self.secondRow.addWidget(self.number9)
-        self.secondRow.addWidget(self.number0)
+        self.secondRow.addWidget(self.numberPlus)
+        self.secondRow.addWidget(self.numberMinus)
 
         self.mainDisplayScreen.addLayout(self.secondRow)
 
@@ -91,18 +99,22 @@ class MainWindow(QMainWindow):
         
         self.numberDecimal = QPushButton(".")
         self.numberDecimal.clicked.connect(self.clickAction)
-        self.numberPlus = QPushButton("+")
-        self.numberPlus.clicked.connect(self.clickAction)
-        self.numberMinus = QPushButton("-")
-        self.numberMinus.clicked.connect(self.clickAction)
+
+        self.number0 = QPushButton("0")
+        self.number0.clicked.connect(self.clickAction)
+
+        self.equals = QPushButton("=")
+        self.equals.clicked.connect(self.clickAction)
+
         self.numberMultiply = QPushButton("*")
         self.numberMultiply.clicked.connect(self.clickAction)
+
         self.numberDevide = QPushButton("/")
         self.numberDevide.clicked.connect(self.clickAction)
 
         self.thirdRow.addWidget(self.numberDecimal)
-        self.thirdRow.addWidget(self.numberPlus)
-        self.thirdRow.addWidget(self.numberMinus)
+        self.thirdRow.addWidget(self.number0)
+        self.thirdRow.addWidget(self.equals)
         self.thirdRow.addWidget(self.numberMultiply)
         self.thirdRow.addWidget(self.numberDevide)
 
@@ -168,7 +180,7 @@ class MainWindow(QMainWindow):
     def backspace(self):
         if self.inputValue is not None:
             self.inputValue = self.inputValue[:-1]
-            self.value.append(str(self.inputValue))
+            self.value = self.value[:-1]
             self.inputField.setText(self.inputValue)
         
     def showValue(self, v):
